5966c4c8220cf25aaf9b85c68c98f6624d116525
Andrei <drei3000@gmail.com>
2016-09-27 17:42:06 -0600

    implemented add, has and get tests

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -6,12 +6,35 @@ import android.test.ActivityInstrumentationTestCase2;
  * Created by drei on 2016-09-27.
  */
 
-public class TweetListTest extends ActivityInstrumentationTestCase2<LonelyTwitterActivity {
+public class TweetListTest extends ActivityInstrumentationTestCase2<LonelyTwitterActivity> {
     public TweetListTest() {
         super(LonelyTwitterActivity.class);
     }
 
     public void testAddTweet() {
         TweetList list = new TweetList();
+        Tweet tweet = new NormalTweet("Hello!");
+        list.add(tweet);
+        assertTrue(list.hasTweet(tweet));
+    }
+
+    public void testHasTweet() {
+        TweetList list = new TweetList();
+        Tweet tweet = new NormalTweet("Hello!");
+        assertFalse(list.hasTweet(tweet));
+        list.add(tweet);
+        assertTrue(list.hasTweet(tweet));
+    }
+
+    public void testGetTweet() {
+        TweetList list = new TweetList();
+        Tweet tweet = new NormalTweet("Hello!");
+        Tweet tweet2 = new NormalTweet("Hi!");
+        list.add(tweet);
+        list.add(tweet2);
+
+        assertEquals(tweet, list.getTweet(0));
+        assertEquals(tweet2, list.getTweet(1));
+
     }
 }
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
@@ -1,7 +1,23 @@
 package ca.ualberta.cs.lonelytwitter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by drei on 2016-09-27.
  */
 public class TweetList {
+    public Object add;
+    List<Tweet> tweets = new ArrayList<Tweet>();
+    public void add(Tweet tweet) {
+        tweets.add(tweet);
+    }
+
+    public boolean hasTweet(Tweet tweet) {
+        return tweets.contains(tweet);
+    }
+
+    public Tweet getTweet(int i) {
+        return tweets.get(i);
+    }
 }
